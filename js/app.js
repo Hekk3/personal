2654147039cc3f0390a48e7e7aac4abb91ea820c
Hekk3 <timurh434@gmail.com>
2021-07-23 06:23:49 +0300

    oh shit, here we go again

--- js/app.js
+++ js/app.js
@@ -36,30 +36,23 @@ window.onload = function() {
 
   let header = document.querySelector('.header');
   let mainRight = document.querySelector('.main-right');
-  let wrapperItemButton = document.querySelectorAll('.aside__tab-button');
-  let wrapperItem = document.querySelectorAll('.wrapper__item');
   const mainRightPaddingTop = 40;
 
   if (header && mainRight) {
 
     mainRight.style.paddingTop = header.offsetHeight + mainRightPaddingTop + 'px';
-
-    for (let i = 0; i < wrapperItemButton.length; i++) {
-
-      wrapperItemButton[i].addEventListener('click', function() {
-
-        for (let i = 0; i < wrapperItem.length; i++) {
-
-          wrapperItem[i].classList.remove('active');
-          wrapperItemButton[i].classList.remove('active');
-        }
-
-        wrapperItem[i].classList.add('active');
-        wrapperItemButton[i].classList.add('active');
-      });
-    }
   }
 
   // content
   // =======
+
+  const copyText = document.querySelector('.ref__heading-link-text');
+
+  if (copyText) {
+
+    copyText.addEventListener('click', function() {
+      copyText.select();
+      document.execCommand("copy");
+    });
+  }
 }
